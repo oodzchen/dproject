a901a056523ebee65c00d61f58e65630e0411306
Kholin <oodzchen@gmail.com>
2023-09-15 16:43:15 +0800

    chore: Add user permission tables

--- config/db/adds.sql
+++ config/db/adds.sql
@@ -13,3 +13,47 @@
 -- created_at TIMESTAMP NOT NULL DEFAULT NOW(),
 -- type react_type
 -- );
+
+CREATE TABLE roles (
+    id INT PRIMARY KEY,
+    front_id VARCHAR(50) NOT NULL,
+    comment VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE permissions (
+    id INT PRIMARY KEY,
+    front_id VARCHAR(50) NOT NULL,
+    comment VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE role_permissions (
+    role_id INTEGER REFERENCES roles(id),
+    permission_id INTEGER REFERENCES permissions(id),
+);
+
+CREATE TABLE user_roles (
+    user_id INTEGER REFERENCES users(id),
+    role_id INTEGER REFERENCES roles(id),
+);
+
+ALTER TABLE users
+ADD COLUMN main_role_id int NOT NULL DEFAULT 1;
+
+ALTER TABLE users
+ADD CONSTRAINT fk_user_role
+FOREIGN KEY (main_role_id)
+REFERENCES roles(id);
+
+INSERT INTO roles SET (front_id, comment) VALUES ('user', 'Normal User');
+INSERT INTO roles SET (front_id, comment) VALUES ('modrator', 'Modrator');
+INSERT INTO roles SET (front_id, comment) VALUES ('admin', 'Website Admin');
+
+INSERT INTO permissions SET (front_id, comment) VALUES ('create_article', 'Create article');
+INSERT INTO permissions SET (front_id, comment) VALUES ('create_reply', 'Create reply');
+INSERT INTO permissions SET (front_id, comment) VALUES ('create_role', 'Create Role');
+INSERT INTO permissions SET (front_id, comment) VALUES ('create_permission', 'Create Permission');
+
+-- INSERT INTO user_roles SET (user_id, role_id) VALUES (1, 1);
+INSERT INTO user_roles (user_id, role_id)
+SELECT id, 1
+FROM users;
