218f9021c3b7d05fe1cf93ba942dc4e42fc8611c
Kholin <oodzchen@gmail.com>
2023-10-18 00:13:17 +0800

    chore: Mail service

--- go.mod
+++ go.mod
@@ -31,6 +31,8 @@ require (
 	github.com/chromedp/cdproto v0.0.0-20230808232040-5d0fb3432de3 // indirect
 	github.com/chromedp/sysutil v1.0.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/emersion/go-sasl v0.0.0-20220912192320-0145f2c60ead // indirect
+	github.com/emersion/go-smtp v0.18.1 // indirect
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/gobwas/ws v1.3.0 // indirect
--- service/mail.go
+++ service/mail.go
@@ -1,4 +1,53 @@
 package service
 
+import (
+	"fmt"
+	"net"
+	"strings"
+
+	"github.com/emersion/go-sasl"
+	"github.com/emersion/go-smtp"
+)
+
 type Mail struct {
+	LoginEmail     string
+	auth           sasl.Client
+	SMTPServer     string
+	SMTPServerPort string
+}
+
+func NewMail(userEmail, password string, smtpServer, smtpServerPort string) *Mail {
+	auth := sasl.NewPlainClient("", userEmail, password)
+
+	return &Mail{userEmail, auth, smtpServer, smtpServerPort}
+}
+
+func (m *Mail) SendVerificationCode(email, code string) error {
+
+	addr := net.JoinHostPort(m.SMTPServer, m.SMTPServerPort)
+	msgTpl := "To: %s\r\n"
+	msgTpl += "From: %s\r\n"
+	msgTpl += "Subject: Verification code\r\n"
+	msgTpl += "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+	msgTpl += "<html><body><h1>Verification code</h1>"
+	msgTpl += "The code is <b>%s</b>\r\n<br>"
+	msgTpl += "For security reasons, please do not disclose the verification code.\r\n<br>"
+	msgTpl += "<hr>Kholin\r\n<br>"
+	msgTpl += "</body></html>"
+	msgData := fmt.Sprintf(msgTpl, email, m.LoginEmail, code)
+
+	// fmt.Println("msg data: ", msgData)
+
+	msg := strings.NewReader(msgData)
+	to := []string{email}
+
+	// fmt.Println("smtp addr: ", addr)
+
+	err := smtp.SendMail(addr, m.auth, m.LoginEmail, to, msg)
+	// err := smtp.SendMail(email, m.auth, m.LoginEmail, to, msg)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
